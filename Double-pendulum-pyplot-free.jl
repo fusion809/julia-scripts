c8c5cb298422d66d2e28db3945319f8e6f152e81
Brenton Horne <brentonhorne77@gmail.com>
2018-11-21 17:48:30 +1000

    d2x_dt->d2x_dt2, d2y_dt->d2x_dt2; + improving comments

--- Double-pendulum-pyplot-free.jl
+++ Double-pendulum-pyplot-free.jl
@@ -12,39 +12,39 @@ function f(t, r)
 	(x, dx, y, dy) = r
 
 	# The double pendulum equations
-	dx_dt  = dx
-	d2x_dt = - ((g * (2 * m1 + m2) * sin(x) + m2 * (g * sin(x-2*y) + 2*(l2 * (dy^2) + l1 * (dx^2) * cos(x-y)) * sin(x-y)))/(2 *l1 * (m1 + m2 - m2 * cos(x-y)^2)))
-	dy_dt  = dy
-	d2y_dt = (((m1 + m2) * (l1 * (dx^2) + g * cos(x)) + l2*m2*(dy^2)*cos(x-y)) * sin(x-y)) / (l2 * (m1 + m2 - m2 * cos(x-y)^2));
+	dx_dt   = dx
+	d2x_dt2 = - ((g * (2 * m1 + m2) * sin(x) + m2 * (g * sin(x-2*y) + 2*(l2 * (dy^2) + l1 * (dx^2) * cos(x-y)) * sin(x-y)))/(2 *l1 * (m1 + m2 - m2 * cos(x-y)^2)))
+	dy_dt   = dy
+	d2y_dt2 = (((m1 + m2) * (l1 * (dx^2) + g * cos(x)) + l2*m2*(dy^2)*cos(x-y)) * sin(x-y)) / (l2 * (m1 + m2 - m2 * cos(x-y)^2));
 
 	# Return the derivatives as a vector
-	[dx_dt; d2x_dt; dy_dt; d2y_dt]
+	[dx_dt; d2x_dt2; dy_dt; d2y_dt2]
 end;
 
 # Constants, I do like that I do not have to parse them manually to ode78
-const m1   = 1
-const m2   = 1
-const g    = 9.8
-const l1   = 1
-const l2   = 1
+const m1    = 1     # Mass of bob 1 in kg
+const m2    = 1     # Mass of pendulum bob 2 in kg
+const g     = 9.8   # Acceleration due to gravity in m/s^2
+const l1    = 1     # Length of pendulum 1 in metres
+const l2    = 1     # Length of pendulum 2 in metres
 
 # Define time vector and interval grid
-const dt   = 0.001
-const tf   = 1000.0
+const dt    = 0.001   # Time increments
+const tf    = 1000.0  # Time at which the simulation ends
 # A column vector containing discrete, dt-spaced, time values between 0 and tf
-t          = 0:dt:tf
+t           = 0:dt:tf
 
 # Initial position in phase space
-const r0   = [pi; 0.0; pi/2; 0.0]
+const r0    = [pi; 0.0; pi/2; 0.0]
 
 # Solve the equations
-(t, pos)   = ode78(f, r0, t)
+(t, pos)    = ode78(f, r0, t)
 
 # Map pos values to x, dx, y and dy
-x          = map(v -> v[1], pos)
-dx         = map(v -> v[2], pos)
-y          = map(v -> v[3], pos)
-dy         = map(v -> v[4], pos);
+x           = map(v -> v[1], pos)
+dx          = map(v -> v[2], pos)
+y           = map(v -> v[3], pos)
+dy          = map(v -> v[4], pos);
 
 # Plots produces non-interative plots, unlike PyPlot
 Pkg.add("Plots")

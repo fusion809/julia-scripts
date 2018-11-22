733b14d4fad9c2aaf39b9048aab304520328fbdb
Brenton Horne <brentonhorne77@gmail.com>
2018-11-22 16:23:26 +1000

    Forced-pendulum: aligning equal signs

--- Forced-pendulum.jl
+++ Forced-pendulum.jl
@@ -10,20 +10,20 @@ function f(t, r)
     (x, dx) = r
 
     # The Lorenz equations
-    dX = dx
-    d2X = -g/l*cos(x) + f0*cos(omega*t)
+    dX      = dx
+    d2X     = -g/l*cos(x) + f0*cos(omega*t)
 
     # Return the derivatives as a vector
     [dX; d2X]
 end;
 
 # Define time vector and interval grid
-const dt = 0.001
-const tf = 100.0
-t = 0:dt:tf
+const dt    = 0.001
+const tf    = 100.0
+t           = 0:dt:tf
 
 # Initial position in space
-const r0 = [0.1; 0.0]
+const r0    = [0.1; 0.0]
 
 # Constants alpha, beta, gamma, delta and omega
 const g     = 9.8
@@ -31,9 +31,9 @@ const l     = 1
 const f0    = 1
 const omega = 1
 
-(t, pos) = ode78(f, r0, t)
-x = map(v -> v[1], pos)
-dx = map(v -> v[2], pos)
+(t, pos)    = ode78(f, r0, t)
+x           = map(v -> v[1], pos)
+dx          = map(v -> v[2], pos)
 
 # Get PyPlot and load it
 Pkg.add("PyPlot")

9956a035d2b6569fa60df12f7b178f75cf2a3a72
Brenton Horne <brentonhorne77@gmail.com>
2018-12-19 09:39:41 +1000

    Minor adjustment to pyplot free double pen; +simple-pendulum.jl;

--- Simple-pendulum.jl
+++ Simple-pendulum.jl
@@ -9,33 +9,27 @@ using ODE;
 
 function f(t, r)
 	# Extract the coordinates from the r vector
-	(x, dx, y, dy) = r
+	(x, dx) = r ;
 
 	# The double pendulum equations
-	dx_dt   = dx
-	d2x_dt2 = - ((g * (2 * m1 + m2) * sin(x) + m2 * (g * sin(x-2*y) + 2*(l2 * (dy^2) + l1 * (dx^2) * cos(x-y)) * sin(x-y)))/(2 *l1 * (m1 + m2 - m2 * cos(x-y)^2)))
-	dy_dt   = dy
-	d2y_dt2 = (((m1 + m2) * (l1 * (dx^2) + g * cos(x)) + l2*m2*(dy^2)*cos(x-y)) * sin(x-y)) / (l2 * (m1 + m2 - m2 * cos(x-y)^2));
-
+	dx_dt   = dx ;
+	d2x_dt2 = - g/l * cos(x);
 	# Return the derivatives as a vector
-	[dx_dt; d2x_dt2; dy_dt; d2y_dt2]
+	[dx_dt; d2x_dt2]
 end;
 
 # Constants, I do like that I do not have to parse them manually to ode78
-const m1    = 1     # Mass of bob 1 in kg
-const m2    = 1     # Mass of pendulum bob 2 in kg
 const g     = 9.8   # Acceleration due to gravity in m/s^2
-const l1    = 1     # Length of pendulum 1 in metres
-const l2    = 1     # Length of pendulum 2 in metres
+const l     = 1     # Length of pendulum 1 in metres
 
 # Define time vector and interval grid
-const dt    = 0.001    # Time increments
-const tf    = 10.0     # Time at which the simulation ends
+const dt    = 1e-5       # Time increments
+const tf    = 1e2        # Time at which the simulation ends
 # A column vector containing discrete, dt-spaced, time values between 0 and tf
 t           = 0:dt:tf
 
 # Initial position in phase space
-const r0    = [pi; 0.0; pi/2; 0.0]
+const r0    = [0.0; 0.0]
 
 # Solve the equations
 (t, pos)    = ode78(f, r0, t)
@@ -44,21 +38,34 @@ N           = length(t)
 
 # Map pos values to x, dx, y and dy
 x           = map(v -> v[1], pos)
-xf          = x[end];
 dx          = map(v -> v[2], pos)
-dxf         = dx[end];
-y           = map(v -> v[3], pos)
-yf          = y[end];
-dy          = map(v -> v[4], pos);
-dyf         = dy[end];
 # Plots produces non-interative plots, unlike PyPlot
-Pkg.add("Plots")
-using Plots;
-plot(x,y)
+Pkg.add("PyPlot")
+Pkg.add("LaTeXStrings")
+
+using PyPlot;
+using LaTeXStrings
+PyPlot.figure(1)
+PyPlot.plot(t,x)
+PyPlot.xlabel(L"$t$")
+PyPlot.ylabel(L"$\theta$")
+PyPlot.figure(2)
+PyPlot.plot(t,dx)
+PyPlot.xlabel(L"$t$")
+PyPlot.ylabel(L"$\dot{\theta}$")
+PyPlot.figure(3)
+PyPlot.plot(x,dx)
+PyPlot.xlabel(L"$\theta$")
+PyPlot.ylabel(L"$\dot{\theta}$")
+PyPlot.figure(4)
+PyPlot.plot3D(t,x,dx)
+PyPlot.xlabel(L"$t$")
+PyPlot.ylabel(L"$\theta$")
+PyPlot.zlabel(L"$\dot{\theta}$")
 
 # Write t, x, dx, y and dy to a file
-A           = hcat(t, x, dx, y, dy);
+A           = hcat(t, x, dx);
 #Af          = A[N,:];
-open("/data/GitHub/mine/scripts/julia-scripts/Double-pendulum-dt-$dt-tf-$tf.txt", "w") do file
+open("/data/GitHub/mine/scripts/julia-scripts/Simple-pendulum-dt-$dt-tf-$tf.txt", "w") do file
 	write(file, "$A");
 end

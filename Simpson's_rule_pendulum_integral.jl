4641043e13f372e5bba1fd270d31b332ff3f2012
Brenton Horne <brentonhorne77@gmail.com>
2020-06-13 03:22:48 +1000

    Improving on Simpson's rule script

--- Simpson's_rule_pendulum_integral.jl
+++ Simpson's_rule_pendulum_integral.jl
@@ -2,8 +2,7 @@
 # simple pendulum that starts with zero velocity at the 
 # positive x-axis to reach the negative x-axis (theta=-pi)
 # using Simpson's rule
-# t is time and y is theta 
-# Import the required NumPy functions
+# t is time and y is theta
 using Pkg;
 
 Pkg.add("SpecialFunctions")
@@ -20,6 +19,7 @@ l=1.0
 
 # Define our integrand
 function f(y)
+    # abs is used below to prevent complex number errors due to the sqrt
     return 1/sqrt(abs(2.0*g/l*sin(y)))
 end
 
@@ -28,7 +28,8 @@ N=1000000000;
 # How close to theta=0 we start our integration and how
 # close to theta=-pi we end our intgration.
 # At theta=0,-pi our function becomes undefined, hence
-# why we cannot start at exact theta=0 or theta=-pi.
+# why we cannot start at exactly theta=0 or end at exactly
+# theta=-pi.
 tol=1/(25.0*N);
 # Integration interval, [y0, yend]
 y0=-tol;
@@ -40,24 +41,40 @@ y=y0;
 
 # Our integration function
 function Simpson(h,y,i,N)
-    if i == 0 || i == N
+    if i == 1 || i == N+1
         return h/3*f(y)
-    elseif (i % 2) == 0
+    elseif (i % 2) == 1
         return 2*h/3*f(y)
     else
         return 4*h/3*f(y)
     end
 end
 
-t = 0;
-# The actual integration
-for i=1:N
-    t = t + Simpson(h,y,i,N);
-    y = y + h
+# Function is used for the integration because otherwise
+# we have to use global t within the loop to prevent the 
+# error mentioned here (
+# https://discourse.julialang.org/t/i-define-t-then-in-a-loop-im-told-its-undefined-why/41258/5)
+# and that is bad for performance. Apparently in Julia 1.5, we 
+# won't need to do this anymore.
+
+function simpson(y0)
+    t = 0.0;
+    y = y0;
+    # The actual integration
+    for i=1:N+1
+        t = t + Simpson(h,y,i,N);
+        if i < N+1
+            y = y + h;
+        end
+    end
+
+    return t
 end
-# Our SciPy approximation to the integral
+
+t=simpson(y0)
+# Our QuadGK approximation to the integral
 T=abs(quadgk(y -> f(y), 0, -pi)[1]);
 
-# Difference between our SciPy approximation and our 
+# Difference between our QuadGK approximation and our 
 # approximation using Simpson's method
 error = abs(T + t)

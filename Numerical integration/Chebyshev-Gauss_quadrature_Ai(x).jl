6bdce0150902c2609d2c2ba821e7a5cf884b2b5a
Brenton Horne <brentonhorne77@gmail.com>
2020-06-15 19:14:41 +1000

    Miscellaneous, largely minor/cosmetic impr to quad scrs

--- Numerical integration/Chebyshev-Gauss_quadrature_Ai(x).jl
+++ Numerical integration/Chebyshev-Gauss_quadrature_Ai(x).jl
@@ -1,17 +1,15 @@
-# b has to be relatively small, as with higher b the result gets very inaccurate, likely due to how much our integrand oscillates
+# b has to be relatively small, as with higher b the result gets very inaccurate, likely due to how much our integrand oscillates with increasing t values
+# Approximates, using Chebyshev-Gauss quadrature, 
+# Ai(y) = \int_0^\infty \cos(t^3/3+y*t) dt
 # Import package manager
 using Pkg;
-
-# Installing required Julia modules
-Pkg.add("LinearAlgebra")
-using LinearAlgebra;
 
 # Required to use the Airy function and its derivative
 Pkg.add("SpecialFunctions")
 using SpecialFunctions;
 
 # Number of steps, excluding starting point
-N                       = 100000000;
+N                       = 1e7;
 # Our truncated integration domain is [a,b]
 a                       = 0.0;
 b                       = 100.0;
@@ -22,9 +20,20 @@ n                       = 1:1:N;
 # Chebyshev-Gauss grid
 tt                      = pi*((2*n.-1)/(2*N));
 x                       = cos.(tt);
+n                       = nothing;
 # Our transformed domain variable
 t                       = (b-a)/2.0*x.+(a+b)/2.0;
+x                       = nothing;
+# Our integrand f(x)
 integrand               = sin.(tt).*cos.((t.^3)/3.0+y*t);
-Ai_Chebyshev_approx     = ((b-a))/(2*N)*sum(integrand);
-Ai_exact                = airyai(y);
-error                   = abs(Ai_Chebyshev_approx-Ai_exact);
+t                       = nothing;
+tt                      = nothing;
+# Chebyshev-Gauss quadrature approximation
+Chebyshev_approx        = ((b-a))/(2*N)*sum(integrand);
+integrand               = nothing;
+# Exact solution is Ai(x)
+exact                   = airyai(y);
+# Absolute difference between Chebyshev approximation and the exact solution
+error                   = abs(Chebyshev_approx-exact);
+
+println("error is $(error)")
--- Numerical integration/Chebyshev-Gauss_quadrature_Gaussian.jl
+++ Numerical integration/Chebyshev-Gauss_quadrature_Gaussian.jl
@@ -1,24 +1,12 @@
 # This approximates
-# \int_0^\infty e^(-t^2) dt
-
-# Import package manager
-using Pkg;
-
-# Installing required Julia modules
-Pkg.add("LinearAlgebra")
-using LinearAlgebra;
-
-# Required to use the Airy function and its derivative
-Pkg.add("SpecialFunctions")
-using SpecialFunctions;
+# \int_0^\infty e^{-t^2} dt
+# Using Chebyshev-Gauss quadrature
 
 # Number of steps, excluding starting point
-N                       = 1000000;
+N                       = 2e8;
 # Our truncated integration domain is [a,b]
 a                       = 0.0;
-b                       = 1000.0;
-# Ai(y) is what we're finding
-y                       = 1.0;
+b                       = 10.0;
 # Column vector of integers from 1 to N
 n                       = 1:1:N;
 # Chebyshev-Gauss grid
@@ -26,10 +14,18 @@ tt                      = pi*((2*n.-1)/(2*N));
 x                       = cos.(tt);
 # Our transformed domain variable
 t                       = (b-a)/2.0*x.+(a+b)/2.0;
-# Our integrand; sin(tt) is a more efficient way to compute sqrt(1-x^2)
+# Free up RAM by clearing x
+x                       = nothing;
+# Our integrand f(x); sin(tt) is a more efficient way to compute sqrt(1-x^2)
 integrand               = sin.(tt).*exp.(-t.^2);
+# Free up RAM by clearing tt, n and t
+tt                      = nothing;
+n                       = nothing;
+t                       = nothing;
 # Chebyshev-Gauss quadrature approximation
 Chebyshev_approx        = ((b-a)*pi)/(2*N)*sum(integrand);
+# Free up RAM by clearing integrand
+integrand               = nothing;
 # Analytical solution to this problem
 exact                   = sqrt(pi)/2;
 # Error in our quadrature approximation of the integral

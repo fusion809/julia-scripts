5ee6d6e7f5bd67a61e5bdacccd36194fd76f110e
Brenton Horne <brentonhorne77@gmail.com>
2021-12-13 00:55:38 +1000

    Rm NaNs from sol, ment higher order methods in docstring, +comments

--- Newton's method/general.jl
+++ Newton's method/general.jl
@@ -9,15 +9,9 @@ the roots.
 """
 function bisection(f, N, a, b)
     x = LinRange(a, b, N+1)
-    change = zeros(size(x))
     fval = f.(x)
-    for i=2:N+1
-        if (sign(fval[i]) + sign(fval[i-1]) == 0)
-            change[i] = 1
-        end
-    end
-
-    initGuess = x[change.==1]
+    xv = x[2:end]
+    initGuess = xv[diff(sign.(fval)).!= 0]
     return initGuess
 end
 
@@ -25,12 +19,14 @@ end
     newtons(f::Function, h::Float, tol::Float, itMax::Integer, 
     initGuess::Vector{Float})
 
-Uses Newton's method to refine initGuess, our initial guess of the root(s) of 
-f, until either itMax iterations has been performed or the relative magnitude 
-of the update Newton's provides to the estimate is below tol. h is the step 
-size used to approximate the derivative.  
+Uses Newton's method (and if needed, up to third order Householder's methods) 
+to refine initGuess, our initial guess of the root(s) of f, until either itMax 
+iterations has been performed or the relative magnitude of the update Newton's 
+provides to the estimate is below tol. h is the step size used to approximate 
+the derivative.  
 """
 function newtons(f, h, tol, itMax, initGuess)
+    # Derivatives
     function fd(x, h)
         return (f(x+h)-f(x-h))/(2*h)
     end
@@ -40,7 +36,9 @@ function newtons(f, h, tol, itMax, initGuess)
     function f3d(x, h)
         return (f(x+2*h)-2*f(x+h)+f(x-h)-f(x-2*h))/(2*h^3)
     end
-    function tolCheck(diff, sol)
+
+    # Check to see if (relative) difference in update is below tol
+    function tolCheck(diff, sol, tol)
         if ! (sol ≈ 0)
             check = abs(diff/sol) > tol
         else
@@ -48,20 +46,25 @@ function newtons(f, h, tol, itMax, initGuess)
         end
         return check
     end
+
+    # Initialize sol
     sol = initGuess
     count = Int64.(zeros(size(initGuess)))
     for j=1:length(initGuess)
         diff = 1
-        while (tolCheck(diff, sol[j]) && count[j] < itMax)
+        while (tolCheck(diff, sol[j], tol) && count[j] < itMax)
             fn = f(sol[j])
             der = fd(sol[j], h)
             der2 = f2d(sol[j], h)
             der3 = f3d(sol[j], h)
             if (der ≈ 0) && (der2 ≈ 0)
+                # Third order Householder's method
                 diff = (6*fn*der^2 - 3*fn^2*der2)/(6*der^3-6*fn*der*der2 + fn^2*der3)
             elseif (der ≈ 0)
+                # Second order, Hailey's method
                 diff = 2*fn*der/(-fn*der2 + 2*der^2)
             else
+                # First order, Newton's method
                 diff = fn/der
             end
             sol[j] -= diff
@@ -71,6 +74,17 @@ function newtons(f, h, tol, itMax, initGuess)
             print("Maximum iterations exceeded and the amount by which ")
             println("Newton's last updated the solution was: ", diff)
         end
+    end
+
+    # Delete NaN entries from sol and corresponding count entries
+    # While loop is used because sol's length will change over this loop
+    j = 1;
+    while j < length(sol)
+        if isnan(sol[j])
+            deleteat!(sol, j)
+            deleteat!(count, j)
+        end
+        j += 1;
     end
     return sol, count
 end
@@ -110,11 +124,12 @@ function f(x)
     return x^4 + 3x^3 + 4x^2 + 3x + 1
 end
 
+# Initialize required variables
 h = 1e-10
 tol = 1e-15
 itMax = 1000
-a = -1000
-b = 1000
+a = -100
+b = 100
 N = 100000
 sol, count = findRoot(f, h, tol, itMax, a, b, N)
 printSoln(sol, count)

b9105aefa834cf96fa055bf91c090a61691924a5
Brenton Horne <brentonhorne77@gmail.com>
2021-12-12 21:38:09 +1000

    80 char limit rule

--- Newton's method/general.jl
+++ Newton's method/general.jl
@@ -2,7 +2,10 @@
 """
     bisection(f::Function, N::Integer, a::Number, b::Number)
 
-Uses the bisection method with N subdivisions of the specified domain [a, b] to find the roots of the function f within said domain. Used to find the initial guess that Newton's method then uses to get a more precise estimate of the roots.
+Uses the bisection method with N subdivisions of the specified domain [a, b] 
+to find the roots of the function f within said domain. Used to find the 
+initial guess that Newton's method then uses to get a more precise estimate of 
+the roots.
 """
 function bisection(f, N, a, b)
     x = LinRange(a, b, N+1)
@@ -22,7 +25,10 @@ end
     newtons(f::Function, h::Float, tol::Float, itMax::Integer, 
     initGuess::Vector{Float})
 
-Uses Newton's method to refine initGuess, our initial guess of the root(s) of f, until either itMax iterations has been performed or the relative magnitude of the update Newton's provides to the estimate is below tol. h is the step size used to approximate the derivative.  
+Uses Newton's method to refine initGuess, our initial guess of the root(s) of 
+f, until either itMax iterations has been performed or the relative magnitude 
+of the update Newton's provides to the estimate is below tol. h is the step 
+size used to approximate the derivative.  
 """
 function newtons(f, h, tol, itMax, initGuess)
     function fd(x, h)
@@ -47,7 +53,10 @@ end
 """
     findRoot(f::Function, h::Float, tol::Float, itMax::Integer, a::Number, b::Number, N::Integer)
 
-Uses the bisection method to get an initial guess of the root(s) of f on the domain [a, b] with N subdivisions, then uses Newton's method with a maximum of itMax iterations and a relative error tolerance of tol. h is the step size used to approximate the derivative. 
+Uses the bisection method to get an initial guess of the root(s) of f on the 
+domain [a, b] with N subdivisions, then uses Newton's method with a maximum of 
+itMax iterations and a relative error tolerance of tol. h is the step size 
+used to approximate the derivative. 
 """
 function findRoot(f, h, tol, itMax, a, b, N)
     initGuess = bisection(f, N, a, b)

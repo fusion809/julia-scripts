c460881698faf2b7ead55abad0dd8993566dd19f
Brenton Horne <brentonhorne77@gmail.com>
2025-06-19 17:44:35 +1000

    Tweaking title for plots

--- ODEs/Generalized/simplePendulum-SYosh.jl
+++ ODEs/Generalized/simplePendulum-SYosh.jl
@@ -1,12 +1,14 @@
 const t0 = 0.0;
-const tf = 200.0;
+const tf = 300.0;
 const dt = 0.0001; 
 using PyPlot;
 const m = 1.0;
 const r = 1.0;
 const g = 9.81;
 const theta0 = 0.0;
-const p0 = 0.0; 
+const p0 = 0.0;
+const k = 1:100; 
+const period = 4*sqrt(r/g)*(pi/2 + 2*pi*sum(exp.(k*pi)./(1 .+ exp.(2*k*pi))))
 include("SYosh.jl")
 include("RKF45.jl")
 
@@ -47,28 +49,29 @@ end
 
 params = (g=g, r=r, m=m);
 conds = @SVector [theta0, p0];
-epsilon = 2e-12;
+epsilon = 1.5e-12;
 t1, theta1, p1 = SYosh(fp, fq, params, t0, tf, conds, dt);
 t2, TH = RKF45(SP, params, t0, tf, conds, epsilon, dt);
 theta2 = TH[:, 1];
 dtheta2 = TH[:, 2];
 
 using PyPlot;
+test = t1 .<= period;
 PyPlot.figure(1)
-PyPlot.plot(t1, theta1)
+PyPlot.plot(t1[test], theta1[test])
 PyPlot.xlabel(L"t")
 PyPlot.ylabel(L"\theta", rotation=0)
-PyPlot.title("Simple pendulum: integrated using Yoshida's 4th-order symplectic method")
+PyPlot.title("Simple pendulum: integrated using Yoshida (1T)")
 PyPlot.figure(2)
-PyPlot.plot(t1, p1)
+PyPlot.plot(t1[test], p1[test])
 PyPlot.xlabel(L"t")
 PyPlot.ylabel(L"p", rotation=0)
-PyPlot.title("Simple pendulum: integrated using Yoshida's 4th-order symplectic method")
+PyPlot.title("Simple pendulum: integrated using Yoshida (1T)")
 PyPlot.figure(3)
 PyPlot.plot(theta1, p1)
 PyPlot.xlabel(L"\theta")
 PyPlot.ylabel(L"p", rotation=0)
-PyPlot.title("Simple pendulum: integrated using Yoshida's 4th-order symplectic method")
+PyPlot.title("Simple pendulum: integrated using Yoshida")
 H1 = Hamiltonian(params, theta1, p1);
 H2 = Hamiltonian_dth(params, theta2, dtheta2);
 PyPlot.figure(4)
